3c04e9611a75afaf9406c7764cc6485273162588
unknown <mail.schaefer.daniel@gmail.com>
2017-08-31 15:40:04 +0200

    Add new files

--- Mobile/Mobile/Mobile.Android/MainActivity.cs
+++ Mobile/Mobile/Mobile.Android/MainActivity.cs
@@ -7,6 +7,9 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using System.Threading.Tasks;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace Mobile.Droid
 {
@@ -24,6 +27,9 @@ namespace Mobile.Droid
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new Mobile.App ());
+
+			MobileCenter.Start("eaf0dde8-6120-4139-b7eb-0b6ed79892ac",
+				   typeof(Analytics), typeof(Crashes));
 
 			ZXing.Net.Mobile.Forms.Android.Platform.Init();
 		}
---
+++ Mobile/MobileTests/Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace MobileTests
+{
+	[TestFixture(Platform.Android)]
+	[TestFixture(Platform.iOS)]
+	public class Tests
+	{
+		IApp app;
+		Platform platform;
+		public string Path { get; }
+
+		public Tests(Platform platform)
+		{
+			this.platform = platform;
+			Path = $"C:\\wc\\MobileCenterTest\\Mobile.Mobile.apk";
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = ConfigureApp.Android.ApkFile(Path).StartApp();
+			//app = AppInitializer.StartApp(platform);
+		}
+
+		[Test]
+		public void AppLaunches()
+		{
+			app.Repl();
+			//app.Screenshot("First screen.");
+		}
+	}
+}
+
